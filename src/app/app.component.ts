74fc4412fcf4a0f9483af0b37196f2a206843be3
Ta Kung <wu.takung@dxc.com>
2021-02-01 20:24:00 +0800

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {Component} from '@angular/core';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FoodNode {
+  name: string;
+  link: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Appcomponent',
+    link: null,
+    children: [
+      {name: 'First', link: '/first-component'},
+      {name: 'Second', link: '/second-component'},
+    ]
+  }, 
+];
+
+/**
+ * @title Tree with nested nodes
+ */
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
